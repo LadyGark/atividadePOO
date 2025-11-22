b5d3c809698296e9f92947146eb3b6d1395e10ce
LadyGark <ellemkesiagarcia@gmail.com>
2025-11-22 17:45:58 -0300

    Initial commit

---
+++ src/Disciplina.java
@@ -0,0 +1,18 @@
+import java.util.List;
+
+public class Disciplina {
+    private String codigo;
+    private String titulo;
+    private int capacidadeMaxima;
+
+
+    List<Aluno> inscritos;
+
+    boolean isLotada()
+
+    List<Aluno> getAlunos()
+
+
+
+
+}
---
+++ src/aluno.java
@@ -0,0 +1,74 @@
+
+
+void main() {
+}
+
+public class Aluno {
+    private String nome;
+    private String email;
+    private int matricula;
+    private String sala;
+    private int periodo;
+
+
+    Aluno (String nome, String email, int matricula, String sala, int periodo){
+        this.nome = nome;
+        this.email = email;
+        this.matricula = matricula;
+        this.sala = sala;
+        this.periodo = periodo;
+
+    }
+
+    Aluno (String nome, String email){
+        this.nome = nome;
+        this.email = email;
+
+    }
+
+    public String Getnome() {
+        return nome;
+    }
+
+    public String Getemail() {
+        return email;
+
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public String getSala() {
+        return sala;
+    }
+
+    public int getPeriodo() {
+        return periodo;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public void setSala(String sala) {
+        this.sala = sala;
+    }
+
+    public void setPeriodo(int periodo) {
+        this.periodo = periodo;
+    }
+
+}
+
+
+
+}
